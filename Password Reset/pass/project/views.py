5471d5f5595a29834631cdc858687fcd9c533971
SANDIP PAL <38961973+sandip308@users.noreply.github.com>
2019-07-20 17:25:20 +0530

    First Commit

---
+++ Password Reset/pass/project/views.py
@@ -0,0 +1,23 @@
+
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm,UserUpdateForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'project/register.html', {'form': form})
+
+def home(request):
+    return render(request,'project/home.html',{})
